85fb9939f1a6f28c2e12ca4dfacdede6205fdb81
=Aashiv <aashivnirmal22@gmail.com>
2026-04-23 23:01:13 +0530

    registration with ajax

--- blog_project/users/views.py
+++ blog_project/users/views.py
@@ -8,16 +8,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout
+from django.http import JsonResponse
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def register_view(request):
+    form = CustomUserCreationForm()
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            login(request, form.save())
-            return redirect('posts:list')
-    else:
-        form = UserCreationForm()
+            user = form.save()
+            login(request, user)            
+            return JsonResponse({"success": True, "message": "User created successfully!"}, status=200)
+        else:
+            # Send form errors back as JSON
+            errors = form.errors.get_json_data()
+            return JsonResponse({"success": False, "errors": errors}, status=400)
+    
     return render(request, 'users/register.html', {'form': form})
 
 def login_view(request):
