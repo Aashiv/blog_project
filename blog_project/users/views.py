8a487a0f513f17f80ebfa8908550c3834ec4ac24
=Aashiv <aashivnirmal22@gmail.com>
2026-04-25 19:55:44 +0530

    user can register and login

--- blog_project/users/views.py
+++ blog_project/users/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout
+from django.urls import reverse
 from django.http import JsonResponse
 
 # Create your views here.
@@ -18,7 +19,11 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return JsonResponse({"success": True, "message": "User created successfully!"}, status=200)
+            message = {
+                'body': 'Congrats!, Your account created successfully.',
+                'url': reverse('posts:list')
+            }
+            return JsonResponse({"success": True, "message": message}, status=200)
         else:
             # Send form errors back as JSON
             errors = form.errors.get_json_data()
@@ -30,8 +35,24 @@ def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
+            name = request.user.get_username()
             login(request, form.get_user())
-            return redirect('posts:list')
+            message = {
+                'body': 'Hi '+name+'. Welcome back!',
+                'url': reverse('posts:list')
+            }
+            return JsonResponse({"success": True, "message": message}, status=200)
+        else:
+            # Send form errors back as JSON
+            errors = {
+                'name_or_pass': 'Incorrect login details.'
+            }
+            return JsonResponse({"success": False, "errors": errors}, status=400)
     else:
         form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('posts:list')
